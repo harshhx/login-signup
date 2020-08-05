96d5b110326d8f8b7d2ac6c33d4c13a42b49cb46
Harsh Gupta <harshgupta07042001@gmaiil.com>
2020-08-06 00:46:12 +0530

    project completed without styling but works perfectly

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// setting up database
+
+mongoose.connect("mongodb://localhost:27017/loginDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+  });
+
+  const User = new mongoose.model("User", userSchema);
+
+
+// get and post requests
+
 app.get("/" , function(req,res){
     res.render("home")
 });
@@ -28,7 +45,44 @@ app.get("/register" , function(req,res){
     res.render("register")
 });
 
+app.get("/afterLogin" , function(req,res){
+    res.render("afterLogin")
+});
 
+app.get("/afterRegister" , function(req,res){
+    res.render("afterRegister")
+})
+
+app.post("/register" , function(req,res){
+   const  username = req.body.username ; 
+   const password = req.body.password;
+   const user = new User({email:username , password: password}) ;
+   user.save(function(err){
+       if(err){
+           console.log(err);
+       }else{
+           res.redirect("/afterRegister")
+       }
+   });
+ 
+});
+
+app.post("/login" , function(req,res){
+    const username = req.body.username ; 
+    const password = req.body.password ;
+    User.findOne({email:username} , function(err , foundUser){
+        if(err){
+            console.log(err);
+        }else if(foundUser){
+            if(foundUser.password === password){
+                res.redirect("/afterLogin")
+            }
+        }
+    })
+
+});
+
+// setting up the server
 
 app.listen(3000 , function(){
     console.log("server is running on port 3000");
